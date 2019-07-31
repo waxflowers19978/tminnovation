6d60accb4b9875b1e9e1347d22bc586d45f91c62
yasuki <shun.210.suke@gmail.com>
2019-07-31 19:50:13 +0900

    first commit

--- tminnovation/settings.py
+++ tminnovation/settings.py
@@ -77,12 +77,28 @@ WSGI_APPLICATION = 'tminnovation.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+     # 'default': {
+     # 'ENGINE': 'django.db.backends.sqlite3',
+     # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     # }
+       'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'gradedb',
+       'USER': 'shunsuke',
+       'PASSWORD': 'shun0210',
+       'HOST': '127.0.0.1',
+       'POST': '5432',
+     }
+}
+
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
