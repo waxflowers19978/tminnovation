42b8b89ffce4ed1907740b718bf91caa21865fd2
yasuki <shun.210.suke@gmail.com>
2019-08-05 15:54:31 +0900

    add gunicorn in settings

--- dev_gouta.py
+++ dev_gouta.py
@@ -1,9 +1,10 @@
-from grade.settings import *
+from tminnovation.settings import *
 
 DEBUG = True
 
 
 
+"""
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -14,3 +15,12 @@ DATABASES = {
          'PORT' : '5432',
      }
  }
+
+"""
+
+ DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
