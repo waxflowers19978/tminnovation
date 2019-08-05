29033f3212d07ced2df6c9fed60885e266815941
goutarouh <gouta.soccer.0220@outlook.jp>
2019-08-05 18:09:15 +0900

    make html

--- tramino/views.py
+++ tramino/views.py
@@ -6,5 +6,29 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 
-def hp(request):
-    return render(request, 'tramino/hp.html')
+def index(request):
+    return render(request, 'tramino/index.html')
+
+def mypage(request):
+    return render(request, 'tramino/mypage.html')
+
+def match_search(request):
+    return render(request, 'tramino/match_search.html')
+
+def match_detail(request):
+    return render(request, 'tramino/match_detail.html')
+
+def event_post(request):
+    return render(request, 'tramino/event_post.html')
+
+def team_search(request):
+    return render(request, 'tramino/team_search.html')
+
+def team_detail(request):
+    return render(request, 'tramino/team_detail.html')
+
+def login(request):
+    return render(request, 'tramino/login.html')
+
+def create(request):
+    return render(request, 'tramino/create.html')
