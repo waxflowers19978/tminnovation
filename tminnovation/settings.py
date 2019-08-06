f02db2f806b2600eaa41d07e065bd05d0e4f4957
yasuki <shun.210.suke@gmail.com>
2019-08-06 22:43:50 +0900

    add whitenoise settings

--- tminnovation/settings.py
+++ tminnovation/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ##############################
 
