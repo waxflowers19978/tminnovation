481285e91403ddb9dc1756088aa267d53a7fbe36
yasuki <shun.210.suke@gmail.com>
2019-08-06 21:55:37 +0900

    change settings tail

--- tminnovation/settings.py
+++ tminnovation/settings.py
@@ -143,6 +143,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ##############################
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+
+
 DEBUG = False
 
 try:
