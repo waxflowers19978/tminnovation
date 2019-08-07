f7c2557a202ecfc90137b30ab6eb25cdc80857c0
goutarouh <gouta.soccer.0220@outlook.jp>
2019-08-07 18:10:22 +0900

    s

--- tminnovation/settings.py
+++ tminnovation/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -156,7 +155,11 @@ ALLOWED_HOSTS = ['*']
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+# STATIC_URL = '/static/'
+STATIC_URL = STATIC_ROOT
+'''
+aiueo
+'''
 
 DEBUG = False
 
