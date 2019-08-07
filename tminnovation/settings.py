ddfa80803d80fc3f30439bf8a78d616680201b84
goutarouh <gouta.soccer.0220@outlook.jp>
2019-08-07 18:14:20 +0900

    s

--- tminnovation/settings.py
+++ tminnovation/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ##############################
 
