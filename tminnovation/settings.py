813ae99671c6acacdf6c0fcb433f0525d4e93213
yasuki <shun.210.suke@gmail.com>
2019-08-17 04:30:21 +0900

    0817-1

--- tminnovation/settings.py
+++ tminnovation/settings.py
@@ -121,6 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'tramino.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- tramino/migrations/0001_initial.py
+++ tramino/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('tramino', '0009_alter_user_last_name_max_length'),
+        ('auth', '0009_alter_user_last_name_max_length'),
     ]
 
     operations = [
