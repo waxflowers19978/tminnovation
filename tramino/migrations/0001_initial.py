096f631636b32afcb88a9ea552449047c07a1e97
yasuki <shun.210.suke@gmail.com>
2019-08-17 04:08:53 +0900

    0817-1

--- tramino/migrations/0001_initial.py
+++ tramino/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0009_alter_user_last_name_max_length'),
+        ('tramino', '0009_alter_user_last_name_max_length'),
     ]
 
     operations = [
